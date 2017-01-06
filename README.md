8b8037dcce7ce577255f146f8ac0b8fac1b44731
zollder <goodzollder@gmail.com>
2017-01-06 03:49:57 -0500

    - Added and configured Mongo DB. - Added model schemas.

--- README.md
+++ README.md
@@ -5,22 +5,49 @@
 - Express
 - Bootstrap
 - Jade
-- MongoDB
 - Mongoose
+- MongoDB
+- Docker
 
 ## Usage
+
+#### Start and configure MongoDB docker container
+```
+sudo apt-get install mongodb-clients
+```
+docker pull mongo
+```
+docker run --name mongodb -d -p 27017:27017 mongo
+```
+mongo localhost
+db.createUser({ user:'<username>', pwd:'<password>', roles:[{role:"userAdminAnyDatabase", db:"admin" }]});
+
+#### Install node dependencies and start application
+```
+npm install
+npm start
+```
+or
+```
+npm install -g nodemon
+nodemon
+
 
 ## Developing
 
 From the command line run:
-	npm install
-	npm start
+
 
 ## Testing
 	
 ## Tools
 
+## Heroku DB:
+your_db_uri => mongodb://<dbuser>:<dbpassword>@<db-host>:<db-port>/<db-name>
+heroku config:set MONGODB_URI=your_db_uri
+
 ## Live demo
 
-https://stark-garden-46120.herokuapp.com
+[Open live demo](https://stark-garden-46120.herokuapp.com)
+
 
---
+++ server/models/db.js
@@ -0,0 +1,78 @@
+var mongoose = require('mongoose');
+var readline = require('readline');
+
+// test using: $ NODE_ENV=production MONGODB_URI=mongodb://<username>:<password>@<host>:<port>/<db-name> nodemon
+var dbUrl = 'mongodb://localhost/mean';
+if (process.env.NODE_ENV === 'production') {
+	dbUrl = process.env.MONGODB_URI;
+}
+mongoose.connect(dbUrl);
+
+// Log Mongoose events
+mongoose.connection.on('connected', function() {
+	console.log('Mongoose connected to ' + dbUrl);
+});
+
+mongoose.connection.on('error', function(error) {
+	console.log('Mongoose connection error: ' + error);
+});
+
+mongoose.connection.on('disconnected', function() {
+	console.log('Mongoose disconnected');
+});
+
+/*
+ * Emulate SIGINT event on Windows OS (default on UNIX systems).
+ */ 
+if (process.platform === "win32") {
+	var rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	
+	rl.on("SIGINT", function() {
+		process.emit ("SIGINT");
+	});
+}
+
+/*
+ * Gracefully shuts down DB connection and invokes provided callback.
+ */
+var shutdown = function(message, callback) {
+	mongoose.connection.close(function() {
+		console.log('Mongoose disconnected via ' + message);
+		callback();
+	});
+};
+
+/*
+ * Captures nodemon restart event (system's SIGUSR2) and closes DB connection.
+ * Kills Node process and emits SIGUSR2 again, once the connection is closed.
+ */
+process.once('SIGUSR2', function() {
+	shutdown('nodemon restart', function() {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+
+/*
+ * Listens to NodeJS process for SIGINT event emitted on application termination.
+ * Gracefully shuts down DB connection and kills the Node process.
+ */
+process.once('SIGINT', function() {
+	shutdown('application termination', function() {
+		process.exit(0);
+	});
+});
+
+/*
+ * Listens to NodeJS process for SIGTERM event emitted on Heroku process termination.
+ * Gracefully shuts down DB connection and kills the Node process.
+ */
+process.once('SIGTERM', function() {
+	shutdown('Heroku process termination', function() {
+		process.exit(0);
+	});
+});
+
+require('./locations-model');
